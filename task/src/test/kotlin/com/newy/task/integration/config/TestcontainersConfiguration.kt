4d99a822e48472be0fd11ba95e8f42885177cde4
Jonghoon Yoon <by.newy@gmail.com>
2026-02-20 20:08:16 +0900

    test: 테스트 환경에서 커스텀 PostgreSQL 이미지 사용하도록 수정

--- task/src/test/kotlin/com/newy/task/integration/config/TestcontainersConfiguration.kt
+++ task/src/test/kotlin/com/newy/task/integration/config/TestcontainersConfiguration.kt
@@ -4,8 +4,14 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
+import org.testcontainers.DockerClientFactory
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.images.builder.ImageFromDockerfile
 import org.testcontainers.mysql.MySQLContainer
 import org.testcontainers.postgresql.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
+import java.nio.file.Paths
+import java.util.concurrent.CompletableFuture
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
@@ -14,13 +20,42 @@ class TestcontainersConfiguration {
     @ServiceConnection
     @Profile("postgresql")
     fun postgresContainer(): PostgreSQLContainer {
-        return PostgreSQLContainer("postgres:16-alpine")
+        val customImageName = "local-task-infra-postgresql:latest"
+        val compatibleName = DockerImageName.parse(customImageName).asCompatibleSubstituteFor("postgres")
+
+        val hasCustomImage = try {
+            DockerClientFactory.instance().client().inspectImageCmd(customImageName).exec()
+            true
+        } catch (_: Exception) {
+            false
+        }
+
+        return if (hasCustomImage) {
+            PostgreSQLContainer(compatibleName)
+        } else {
+            PostgreSQLContainer(compatibleName).apply {
+                val imageDefinition = ImageFromDockerfile(customImageName, false)
+                    .withDockerfile(Paths.get("..", "local-infra", "Dockerfile"))
+                setImage(CompletableFuture.completedFuture(imageDefinition.get()))
+            }
+        }.apply {
+            setWaitStrategy(Wait.forListeningPort())
+            withEnv("POSTGRES_INITDB_ARGS", "--encoding=UTF8")
+        }
     }
 
     @Bean
     @ServiceConnection
     @Profile("mysql")
     fun mysqlContainer(): MySQLContainer {
-        return MySQLContainer("mysql:8.0")
+        return MySQLContainer("mysql:8").apply {
+            withCommand(
+                "mysqld",
+                "--character-set-server=utf8mb4",
+                "--innodb_ft_min_token_size=1",
+                "--ft_min_word_len=1",
+                "--ngram_token_size=1",
+            )
+        }
     }
 }
